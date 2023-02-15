9472a67a399d8c889d8f061f501dfb17597e8b1c
Hyejun Kim <hyejun2122@gmail.com>
2023-02-16 10:43:02 +1100

    Initial Commit

---
+++ DreamBoat/DreamBoatApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamBoatApp.swift
+//  DreamBoat
+//
+//  Created by Hyejun Kim on 16/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct DreamBoatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
